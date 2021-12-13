13c3c2f11e11fb30f0123b848dd6c3363e53d72d
Juan <[judagosa8520@gmail.com]>
2021-12-12 20:56:45 -0500

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import './styles.css';
+
+import { Todo, TodoList } from './classes';
+import { crearTodoHtml } from './js/componentes';
+
+
+export const todoList = new TodoList();
+
+todoList.todos.forEach(todo => crearTodoHtml(todo) );
+
+
+todoList.todos[0].imprimirClase();
+
+
+console.log( 'todos', todoList.todos );
